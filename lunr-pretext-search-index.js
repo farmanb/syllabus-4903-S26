21cbc25c97e40a326847c9934be29a34c65a031a
blake-farman <blake.farman@gmail.com>
2026-02-12 12:34:46 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,95 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright   "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website     Office  Nethken 235       Office Hours     Days  TIME PM  TIME PM       Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 4903 001)    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 4903 001  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  Course description goes here    Course Prerequisites  Prerequisites go here.    Student Learning Outcomes  Student Learning Outcomes go here    Course Topics  Course topics go here.    Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation   Exams  There will be two in-class exams and a cumulative final exam. The exams are scheduled as follows:     Exam  Date    Exam 1  Day of Week, Month Day, 2026    Exam 2  Day of Week, Month Day, 2026    Final Exam  Day of Week, Month Day, 2026         The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.        Participation and Engagement  Active participation is essnential for success in this course. Students are expected to attend all classes, contribute to discussions, and engage in collaborative activities. This includes asking questions, sharing insights, and working effectively during in-class exercises or group work.  Constructive engagement allows students to deepen their understanding of the material, learn from their peers, and build essential problem-solving and communication skills that will be valuable in future coursework and careers.  Graduate students will take on leadership roles during team-based projects, mentoring undergraduate teammates, facilitating collaboration, and managing project milestones. This experience is designed to enhance both their understanding of the material and their ability to communicate and guide others effectively, which are essential skills for advanced study and professional growth.    Homework  Homework assignments are a critical component of the course. They are designed to reinforce the concepts covered in lectures and provide hands-on experience. Assignments will include a mix of logical proofs, programming tasks, and conceptual exercises. Solutions must be submitted on time and will be graded for clarity and correctness. Collaboration is encouraged, but each student must submit their own work.  In addition to the core problems assigned to all students, graduate students will tackle advanced exercises that require a deeper understanding of the material and the ability to explore alternative approaches.  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the material. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should ensure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.    Quizzes  Quizzes will be administered periodically to assess understanding of key concepts and ensure students are keeping up with the material. These will be short, focused assessments designed to reinforce learning and identify areas for improvement. They are an opportunity to practice applying course concepts and receive feedback on your progress.    Weights  Grades will be calculated using the weights below.   Evaluation Criteria    Grade Category  Weight    Homework  30%    Exam 1  20%    Exam 1  20%    Final Exam  30%       Grading Scale  Letter grades will be assigned based on the scale below.   Grading Scale    Average  Letter Grade     A     B     C     D     F      "
+},
+{
+  "id": "exams-4",
+  "level": "2",
+  "url": "evaluation.html#exams-4",
+  "type": "Warning",
+  "number": "3.1",
+  "title": "",
+  "body": "    The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook   Ideals, Varieties, and Algorithms , 5th edition, by Authors, ISBN 978-3-031-91840-7.     Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of one missed exam due to absence, the Final Exam can be used to replace the missing exam. Further missed exams will result in a grade of zero. If a University sponsored activity requires you to miss an exam, you must complete the exam in advance.         Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.    Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Division of Student Affairs; Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Drop with a W date  The latest date a student may drop a course (or resign his\/her entire schedule) with a W grade for Spring 2026 is Friday, May 1, 2026, 2026.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Schedule",
+  "body": " Schedule   March 9 March 15     March 11  Material       March 16 March 22     March 16  Material    March 18  Material       March 23 March 29     March 23  Material    March 25  Material       March 30 April 5     March 30  Material    April 01  Material       April 6 April 12     April 08  Material       April 13 April 19     April 13  Material    April 15  Material       April 20 April 26     April 20  Material    April 22  Material       April 27 May 3     April 27  Material    April 29  Material       May 4 May 10     May 04  Material    May 06  Material       May 11 May 17     May 11  Material    May 13  Material       May 18 May 24     May 18  Material    May 20  Material        The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "6.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
